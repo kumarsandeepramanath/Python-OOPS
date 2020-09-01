6854eea50e791b34b000cc57c5b35cf953696597
unknown <kumarsandeepr89@gmail.com>
2020-09-02 00:11:26 +0530

    Exercise file commited

--- Exercise1.py
+++ Exercise1.py
@@ -1,10 +1,19 @@
 class PlayerCharacter:
+  #Class Object Attribute
+  membership = True
   def __init__(self,name):
-    self.name = name
+    if(PlayerCharacter.membership):
+      self.name = name
+      
+  @classmethod
+  def calculate_sum(cls,num1,num2):
+    return num1+num2
   
   def run(self):
     print("run")
+    print(f"My name is {PlayerCharacter.membership}")
 player1 = PlayerCharacter("Sandeep")
-# print(player1.run())
-print(player1)
+print(player1.run())
+print(PlayerCharacter.calculate_sum(5,2))
+# print(player1)
     
