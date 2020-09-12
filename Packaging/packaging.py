090d75daedf351f3bf75affe1967950c5190b7e0
unknown <kumarsandeepr89@gmail.com>
2020-09-12 22:52:18 +0530

    Modules

---
+++ Packaging/packaging.py
@@ -0,0 +1,2 @@
+def what_is_packaging():
+    return "packaging"
---
+++ main.py
@@ -0,0 +1,7 @@
+# import utility
+from utility import multiply
+# import Packaging.packaging
+from Packaging import packaging
+
+print(multiply(2,3))
+print(packaging.what_is_packaging())
