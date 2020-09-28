6b824dadba3f7762e3cb094bb14148da15cdd62b
unknown <kumarsandeepr89@gmail.com>
2020-09-28 22:32:03 +0530

    About web scraping

--- Web_Scrapping/web_scrapping.py
+++ Web_Scrapping/web_scrapping.py
@@ -1,1 +1,10 @@
-print("web scrapping")
+# print("web scrapping")
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/news')
+# print(res.text)
+soup = BeautifulSoup(res.text,'html.parser')
+links = soup.select('.storylink')[0]
+votes = soup.select('.score')
+print(soup.select('.storylink')[0])
