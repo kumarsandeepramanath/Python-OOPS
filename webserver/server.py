b89ce16b3e0378059cf2e94ab4025c50ce66e59c
unknown <kumarsandeepr89@gmail.com>
2020-10-17 20:10:36 +0530

    Added the templates file for the beautiful website

--- webserver/server.py
+++ webserver/server.py
@@ -12,11 +12,36 @@ def good_morning(username=None,post_id=None):
     # return 'Hello, Sany!'
     return render_template('index.html',name=username,post_id=post_id)
 
-@app.route('/about')
-def about():
+@app.route('/<string:page_name>')
+def html_page(page_name:None):
     # return 'Hello, Sany!'
-    return render_template('about.html')
+    return render_template(page_name)
 
-@app.route('/blog')
-def blog():
-    return 'Hello, In the BLOG!'
+# @app.route('/index.html')
+# def index():
+#     # return 'Hello, Sany!'
+#     return render_template('index.html')
+
+# @app.route('/about.html')
+# def about():
+#     # return 'Hello, Sany!'
+#     return render_template('about.html')
+
+# @app.route('/components.html')
+# def components():
+#     # return 'Hello, Sany!'
+#     return render_template('components.html')
+
+# @app.route('/contact.html')
+# def contact():
+#     # return 'Hello, Sany!'
+#     return render_template('contact.html')
+
+# @app.route('/works.html')
+# def works():
+#     # return 'Hello, Sany!'
+#     return render_template('works.html')
+
+# @app.route('/blog')
+# def blog():
+#     return 'Hello, In the BLOG!'
